69f9a3a22690d10f71c561bdfef385b4ccd05813
bhavinbhuta <bhuta.bhavin1994@gmail.com>
2017-06-27 21:36:42 -0400

    Update Ocean.java

--- Ocean.java
+++ Ocean.java
@@ -1,15 +1,4 @@
-/**
- * File: Ocean.java
- * 
- * @version 1.0
- * 
- * @author Sanjay Haresh Khatwani (sxk6714)
- * @author Bhavin Sanjay Bhuta (bsb5375)
- * 
- *         This file initiaises the ocean, prints the ocean and adds ship
- *         according to it's orientation.
- * 
- */
+
 public abstract class Ocean {
 
 	protected String[][] ocean;
